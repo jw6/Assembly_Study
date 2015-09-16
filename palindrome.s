413c2662cafe6e8dd10f5f8f72ee3a1af69a46da
Jay Wang <jaywang007@yahoo.com>
2015-09-16 13:38:33 -0400

    Wednesday 2st commit

--- palindrome.s
+++ palindrome.s
@@ -9,7 +9,7 @@ fir_msg: .asciiz		"Programming assignment 1 for CDA3101\n"
 
 sec_msg: .asciiz		"This palindrome checker only deals with positive integer number.\n"
 
-third_msg: .asciiz		"Enter a number to check if it is a palindrome or not.\n"
+input_msg: .asciiz		"Enter a number to check if it is a palindrome or not.\n"
 
 isPalindrome_msg: .asciiz		"is a palindrome number.\n"
 
@@ -19,6 +19,18 @@ NotPalindrome_msg: .asciiz		"is not a palindrome number.\n"
 		.text
 main:
 		li		$t0, 0			#load 0 into $t0 reverse
+
+		la		$a0, fir_msg
+		li		4
+		syscall
+
+		la		$a0, sec_msg
+		li		4
+		syscall
+
+		la		$a0, input_msg
+		li		4
+		syscall 
 
 		li		$v0, 5			#load system read_int call into v5
 		syscall						#make the system call
