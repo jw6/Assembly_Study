ef8736f03a5e3d9532ec19a0f249d3c4612ccad8
Jay Wang <jaywang007@yahoo.com>
2015-09-16 12:16:05 -0400

    Wednesday 1st commit

---
+++ palindrome.s
@@ -0,0 +1,25 @@
+#Jiaying Wang -- 09/15/15
+#palindrome.s -- A MIPS palindrome integer checker
+#Registers used:
+#		v0		- system parameter and return value
+#		a0		- system parameter --strings to print
+#		t0		-	used to hold value of reverse
+.data
+fir_msg: .asciiz		"Programming assignment 1 for CDA3101\n"
+
+sec_msg: .asciiz		"This palindrome checker only deals with positive integer number.\n"
+
+third_msg: .asciiz		"Enter a number to check if it is a palindrome or not.\n"
+
+isPalindrome_msg: .asciiz		"is a palindrome number.\n"
+
+NotPalindrome_msg: .asciiz		"is not a palindrome number.\n"
+
+
+		.text
+main:
+		li		$t0, 0			#load 0 into $t0 reverse
+
+		li		$v0, 5			#load system read_int call into v5
+		syscall						#make the system call
+		move	$t1, $v0		#move the number read
