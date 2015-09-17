ec2708539f090eb95fe1c2cd166706411565eb4f
Jay Wang <jaywang007@yahoo.com>
2015-09-17 00:34:06 -0400

    Wednesda 3st commit

--- palindrome.s
+++ palindrome.s
@@ -5,7 +5,8 @@
 #		a0		- system parameter --strings to print
 #		t0		-	used to hold value of reverse
 #		t1		- used to hold value of num
-#
+#		t2		- used to hold value of temp
+
 .data
 fir_msg: .asciiz		"Programming assignment 1 for CDA3101\n"
 
@@ -13,15 +14,15 @@ sec_msg: .asciiz		"This palindrome checker only deals with positive integer numb
 
 input_msg: .asciiz		"Enter a number to check if it is a palindrome or not.\n"
 
-isPalindrome_msg: .asciiz		"is a palindrome number.\n"
+Is_Pal_msg: .asciiz		"is a palindrome number.\n"
 
-NotPalindrome_msg: .asciiz		"is not a palindrome number.\n"
+Not_Pal_msg: .asciiz		"is not a palindrome number.\n"
 
 
 		.text
 main:
 		li		$t0,	0			#load 0 into $t0 reverse
-
+		li		$t8,	10		#load 10 into $t8
 		##print the welcome message
 		la		$a0,	fir_msg
 		li		$v0,	4
@@ -30,19 +31,62 @@ main:
 		la		$a0,	sec_msg
 		li		$v0,	4
 		syscall
-		
+
 		##print the read_int message
-		la		$a0, input_msg
+		la		$a0,	input_msg
 		li		$v0,	4
 		syscall
 
-		li		$v0, 5			#load system read_int call into v5
+		li		$v0, 5			#load system read_int call into v0
 		syscall						#make the system call
 		move	$t1,	$v0		#move the number read, store num
-		move	$t2,	$t1		#temp = num
+
+		move	$t2,	$t1		#copy the value of num into temp
+
+		jal while_loop
 
 while_loop:
-		beq		$t2,	0,	endloop		#if temp = 0, end of loop
-		mul		$t1,	10		#otherwise,
+		blez	$t2,	endloop			#check if temp equals 0
+
+		mult	$t0,	$t8	#reverse multply by 10
+		mflo	$t3					#temp value to hold result
+		move	$t0,	$t0		#move the value into reverse
+
+		div		$t2,	$t8		#temp divided by 10
+		mfhi	$t3					#temp value to hold remainder temp % 10
+		mflo	$t2					#temp value to hold t2
+
+		add		$t0,	$t0,	$t3		#reverse = reverse + temp % 10
+
+		b		while_loop
 
 endloop:
+		beq		$t1,	$t2,	Is_Pal
+		bne		$t1,	$t2,	Not_Pal
+
+
+Is_Pal:
+		la		$a0,	$t0
+		li		$v0,	1
+		syscall
+
+		la		$a0,	Is_Pal_msg
+		li		$v0,	4
+		syscall
+
+		j		Exit
+
+Not_Pal:
+		la		$a0,	$t0
+		li		$v0,	1
+		syscall
+
+		la		$a0,	Not_Pal_msg
+		li		$v0,	1
+		syscall
+
+		j		Exit
+
+Exit:
+		li	$v0,	10
+		syscall
